54c724a6dc616c6d7355f178d01f18529e815b5f
Jonathan Kornich <36587591+jonathan-kornich@users.noreply.github.com>
2018-11-17 17:50:05 -0800

    Deleted unused code.

--- Software/Base/base-logic.c
+++ Software/Base/base-logic.c
@@ -9,11 +9,6 @@
 
 #define Bit(b) ( 1U<<b )
 
-void outchar(char c)
-{
-	while ((UART5->FR & 0x00000020) != 0);
-	UART5->DR = c;
-}
 
 PWM_ servos[5] = {M0PWM0_, M0PWM1_, M1PWM4_, M1PWM6_, M0PWM6_0_};
 
@@ -31,27 +26,7 @@ void setup(void)
 	}
 #endif
 	
-#ifdef TEST_UART
-// PE5, UTx5
-	SYSCTL->RCC &= ~(0x400000); // Don't divide system clock.
-
-	SYSCTL->RCGCUART |= Bit(5);
-	SYSCTL->RCGCGPIO |= Bit(4);
-	while((SYSCTL->PRGPIO&0x01) == 0){}
-	GPIOE->AMSEL &= ~0b00110000;
-	GPIOE->AFSEL |= 0b00110000;
-	GPIOE->PCTL |= (GPIOE->PCTL&0xFF00FFFF)+0x00220000;
-	GPIOE->DEN |= 0b00110000;
-	
-	UART5->CTL &= ~Bit(0);
-
-	UART5->IBRD = 104;
-	UART5->FBRD = 11;
-
-	UART5->LCRH = 0b01110000; // FIFO enable, 8-bit data
-//	UART5->CC = 0b0000;       // Use normal clock.
-	UART5->CTL |= 0x301;
-	
+#ifdef TEST_ADC
 #endif
 }
 
@@ -74,7 +49,6 @@ void loop(void)
 	}
 #endif
  
-#ifdef TEST_UART
-outchar('J');
+#ifdef TEST_ADC
 #endif
 }
