9c1c9c250cf6431b9303a1b10e938f33656f9ba2
Jonathan Kornich <36587591+jonathan-kornich@users.noreply.github.com>
2018-11-26 16:29:45 -0800

    Move base code to base-logic.c.

--- Software/Base/base-logic.c
+++ Software/Base/base-logic.c
@@ -3,52 +3,93 @@
 #include "../Libraries/PWM.h"
 #include "../Libraries/SysTick.h"
 
+#include "../Libraries/UART.h"
+#include "../Libraries/LCD.h"
+
 #include <tm4c123gh6pm.h>
 
-#define TEST_PWM
+typedef uint8_t byte;
 
-#define Bit(b) ( 1U<<b )
+byte frame[1 + 5] = {0};
+byte data[5] = {0};
 
+unsigned ByteToPercent(byte b)
+{
+	return 100.0 / 255.0 * b;
+}
 
-PWM_ servos[5] = {M0PWM0_, M0PWM1_, M1PWM4_, M1PWM6_, M0PWM6_0_};
+void Frame_Recieve(byte * frame, unsigned data_size)
+{
+	char c;
+	do
+	{
+		c = UART_ReadChar(UART4_);
+	}
+	while (c != 0xDA);
+	frame[0] = c;
+	for (int i = 1; i < 1 + data_size; i++)
+	{
+		frame[i] = UART_ReadChar(UART4_);
+	}
+}
+
+void Frame_Unpack(byte * frame, byte * data, unsigned data_size)
+{
+	for (int i = 0; i < data_size; i++)
+	{
+		data[i] = frame[1 + i];
+	}
+}
 
 void setup(void)
 {
-#ifdef TEST_QEI
-	QEI_Init(QEI1_, 24, 4);
-#endif
-	
-#ifdef TEST_PWM
 	SysTick_Init();
-	for (int i = 0; i < 5; i++)
-	{
-		PWM_Init(servos[i], 50);
-	}
-#endif
-	
-#ifdef TEST_ADC
-#endif
+	LCD_Init(LCD0_, UART5_, 9600);
+	UART_Init(UART4_, true, false, 9600);
+	LCD_ClearScreen(LCD0_);
 }
 
 void loop(void)
 {
-#ifdef TEST_QEI
-	volatile unsigned value = QEI_Position(QEI1_);
-#endif
-	
-#ifdef TEST_PWM
-	for (int i =0; i < 5; i++)
+	Frame_Recieve(frame, 5);
+	Frame_Unpack(frame, data, 5);
+	LCD_ClearScreen(LCD0_);
+	for (int i = 0; i < 5; i++)
 	{
-		PWM_Position(servos[i], 18000);
-		SysTick_WaitSeconds(1);
+		LCD_PrintChar(LCD0_, i + 'A');
+		int n = LCD_PrintNumber(LCD0_, ByteToPercent(data[i]));
+		for (int j = 0; j < 5-n; j++)
+		{
+			LCD_PrintChar(LCD0_, ' ');
+		}
 	}
-	for (int i = 4; i >= 0; i--)
-	{
-		PWM_Position(servos[i], 19000);
-		SysTick_WaitSeconds(1);
-	}
-#endif
- 
-#ifdef TEST_ADC
-#endif
+	SysTick_WaitMilliseconds(500);
+	UART_ClearFIFO(UART4_);
 }
+
+// #define Bit(b) ( 1U<<b )
+//
+// PWM_ servos[5] = {M0PWM0_, M0PWM1_, M1PWM4_, M1PWM6_, M0PWM6_0_};
+//
+// void setup(void)
+// {
+// 	SysTick_Init();
+// 	for (int i = 0; i < 5; i++)
+// 	{
+// 		PWM_Init(servos[i], 50);
+// 	}
+// }
+//
+// void loop(void)
+// {
+// 	for (int i =0; i < 5; i++)
+// 	{
+// 		PWM_Position(servos[i], 18000);
+// 		SysTick_WaitSeconds(1);
+// 	}
+// 	for (int i = 4; i >= 0; i--)
+// 	{
+// 		PWM_Position(servos[i], 19000);
+// 		SysTick_WaitSeconds(1);
+// 	}
+// }
--- Software/Glove/glove-logic.c
+++ Software/Glove/glove-logic.c
@@ -4,13 +4,9 @@
 #include "../Libraries/UART.h"
 #include "../Libraries/LCD.h"
 
-//#include <tm4c123gh6pm.h>
-
-#define TIVA_RX
 
 typedef uint8_t byte;
 
-#ifdef TIVA_TX
 uint32_t raw[5] = {0};
 byte data[5] = {0};
 byte frame[1 + 5] = {0};
@@ -41,83 +37,19 @@ void Frame_Send(byte * frame, unsigned data_size)
 		UART_WriteChar(UART4_, frame[i]);
 	}
 }
-#endif
 
-#ifdef TIVA_RX
-byte frame[1 + 5] = {0};
-byte data[5] = {0};
-
-unsigned ByteToPercent(byte b)
-{
-	return 100.0 / 255.0 * b;
-}
-
-void Frame_Recieve(byte * frame, unsigned data_size)
-{
-	char c;
-	do
-	{
-		c = UART_ReadChar(UART4_);
-	}
-	while (c != 0xDA);
-	frame[0] = c;
-	for (int i = 1; i < 1 + data_size; i++)
-	{
-		frame[i] = UART_ReadChar(UART4_);
-	}
-}
-
-void Frame_Unpack(byte * frame, byte * data, unsigned data_size)
-{
-	for (int i = 0; i < data_size; i++)
-	{
-		data[i] = frame[1 + i];
-	}
-}
-#endif
 
 void setup(void)
 {
-#ifdef TIVA_TX
 	ADC_Init(raw, 5);
 	SysTick_Init();
 	UART_Init(UART4_, false, true, 9600);
-#endif
-
-#ifdef TIVA_RX
-	SysTick_Init();
-	LCD_Init(LCD0_, UART5_, 9600);
-	UART_Init(UART4_, true, false, 9600);
-	LCD_ClearScreen(LCD0_);
-#endif
 }
 
 void loop(void)
 {
-#ifdef TIVA_TX
-	
 	ADC_Read();
 	Glove_RawToFrameableData(raw, data, 5);
 	Frame_Pack(frame, data, 5);
 	Frame_Send(frame, 5);
-//	SysTick_WaitMilliseconds(100);
-#endif
-
-#ifdef TIVA_RX
-	Frame_Recieve(frame, 5);
-	Frame_Unpack(frame, data, 5);
-	for (int i = 0; i < 5; i++)
-	{
-		LCD_PrintChar(LCD0_, i + 'A');
-		int n = LCD_PrintNumber(LCD0_, ByteToPercent(data[i]));
-		for (int j = 0; j < 5-n; j++)
-		{
-			LCD_PrintChar(LCD0_, ' ');
-		}
-	}
-	SysTick_WaitMilliseconds(100);
-	UART_ClearFIFO(UART4_);
-	LCD_ClearScreen(LCD0_);
-	
-#endif
 }
